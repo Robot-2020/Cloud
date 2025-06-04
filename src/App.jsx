ea5fc36badcdfb0fdbf1039be4607f769a812c19
Robot-2020 <1055570860@qq.com>
2025-06-04 14:58:03 +0800

    加载页实现

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import './App.css'
+import { useRef, useEffect } from 'react'
+import Loader from './components/loader/Index';
+// import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+// import 'locomotive-scroll/dist/locomotive-scroll.css'
 
-const App = () => {
+
+
+function App() {
+  const containerRef = useRef(null)
+  // useEffect(() => {
+  //   // Update the locomotive scroll whenever the location changes
+  //   if (containerRef.current) {
+  //     const event = new Event('resize');
+  //     window.dispatchEvent(event);
+  //   }
+  // }, [location]);
+  
   return (
-    <div>
-      App
-    </div>
+    // <LocomotiveScrollProvider
+    // options={
+    //   {
+    //     smooth: true,
+    //     // ... all available Locomotive Scroll instance options 
+    //   }
+    // }
+    // watch={
+    //   [
+    //     //...all the dependencies you want to watch to update the scroll
+    //   ]
+    // }
+    
+    // containerRef={containerRef}
+    // >
+      <div ref={containerRef} className='main text-white overflow-hidden'>
+        <Loader />
+      </div>
+    // </LocomotiveScrollProvider>
+    
   )
 }
 
